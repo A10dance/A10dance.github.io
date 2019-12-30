379161013a0832c9f927f0c9caa1ebea3b42b11a
hadar abc <hadar708090@gmail.com>
2019-12-30 12:55:50 +0200

    .

--- main.js
+++ main.js
@@ -22,19 +22,72 @@ function markA10dance() {
     alert(`Your attendance was marked successfully, ${username.value}!`);
 }
 
-function enableBT() {
-    // For earlier versions of Windows and Linux, you still have to go to 
-    // chrome://flags/#enable-experimental-web-platform-features, 
-    // enable the highlighted flag, and restart Chrome for now.
+// function enableBT() {
+//     // For earlier versions of Windows and Linux, you still have to go to 
+//     // chrome://flags/#enable-experimental-web-platform-features, 
+//     // enable the highlighted flag, and restart Chrome for now. 
 
+//     navigator.bluetooth.requestDevice({
+//             acceptAllDevices: true,
+//             optionalServices: ['battery_service']
+//         })
+//         .then(device => {
+//             alert(device);
+//         })
+//         .catch(error => {
+//             alert(error);
+//         });
+// }
+
+
+
+
+
+function enableBT() {
+
+    console.log('Requesting any Bluetooth Device...');
     navigator.bluetooth.requestDevice({
+            // filters: [...] <- Prefer filters to save energy & show relevant devices.
             acceptAllDevices: true,
             optionalServices: ['battery_service']
         })
         .then(device => {
-            alert(device);
+            console.log('Connecting to GATT Server...');
+            return device.gatt.connect();
+        })
+        .then(server => {
+            // Note that we could also get all services that match a specific UUID by
+            // passing it to getPrimaryServices().
+            console.log('Getting Services...');
+            return server.getPrimaryServices();
+        })
+        .then(services => {
+            console.log('Getting Characteristics...');
+            let queue = Promise.resolve();
+            services.forEach(service => {
+                queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+                    console.log('> Service: ' + service.uuid);
+                    characteristics.forEach(characteristic => {
+                        console.log('>> Characteristic: ' + characteristic.uuid + ' ' +
+                            getSupportedProperties(characteristic));
+                    });
+                }));
+            });
+            return queue;
         })
         .catch(error => {
-            alert(error);
+            console.log('Argh! ' + error);
         });
+}
+
+/* Utils */
+
+function getSupportedProperties(characteristic) {
+    let supportedProperties = [];
+    for (const p in characteristic.properties) {
+        if (characteristic.properties[p] === true) {
+            supportedProperties.push(p.toUpperCase());
+        }
+    }
+    return '[' + supportedProperties.join(', ') + ']';
 }
