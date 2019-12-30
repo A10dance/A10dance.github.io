a237166b8e2cc1a403b436d360c16e9b342e2edd
hadar abc <hadar708090@gmail.com>
2019-12-30 11:41:14 +0200

    .

---
+++ main.js
@@ -0,0 +1,41 @@
+// For chrome remote debugging:
+// change the USB connection mode to "Audio Source"
+// (Phone Settings > Developer Options > USB Configuration).
+
+let hasWebStorage = true;
+let username;
+
+function onload() {
+    if (typeof (Storage) == "undefined") {
+        alert("Sorry! No Web Storage support..");
+        hasWebStorage = false;
+        return;
+    }
+    username = document.getElementById("username");
+    username.value = localStorage.getItem("username");
+}
+
+function markA10dance() {
+    if (hasWebStorage) {
+        localStorage.setItem("username", username.value);
+    }
+    alert(`Your attendance was marked successfully, ${username.value}!`);
+}
+
+function enableBT() {
+    // For earlier versions of Windows and Linux, you still have to go to 
+    // chrome://flags/#enable-experimental-web-platform-features, 
+    // enable the highlighted flag, and restart Chrome for now.
+
+    navigator.bluetooth.requestDevice({
+            filters: [{
+                services: ['battery_service']
+            }]
+        })
+        .then(device => {
+            alert(device);
+        })
+        .catch(error => {
+            alert(error);
+        });
+}
